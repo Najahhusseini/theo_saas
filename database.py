6d84ba4a7ee3699d0f6be24a48444761b5c71e77
Najah <najah.husseini@gmail.com>
2026-03-01 20:43:18 +0700

    Updated database to use Railway DATABASE_URL

--- database.py
+++ database.py
@@ -1,8 +1,35 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:theo123@localhost:5432/theo_saas"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("Loaded DB URL:", DATABASE_URL)  # temporary debug line
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+from sqlalchemy.orm import Session
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 engine = create_engine(DATABASE_URL)
 
